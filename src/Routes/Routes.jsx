caa84eae80f8ef8771a51a6d016bda8e3cf75659
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-10 19:49:59 +0600

    added about section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Header from "../Pages/Shared/Header/Header";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
