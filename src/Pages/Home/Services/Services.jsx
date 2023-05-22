67096e6404899fc92de36111b0aa2c8ab7e5bba7
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-22 21:48:29 +0600

    added sort and search

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ServiceCard from './ServiceCard';
 import { HiClock, } from "react-icons/hi2";
 import { FaCalendarAlt, FaCommentDots, FaMapMarkerAlt, FaPhoneAlt, FaRegCircle } from "react-icons/fa";
@@ -6,13 +6,21 @@ import { FaCalendarAlt, FaCommentDots, FaMapMarkerAlt, FaPhoneAlt, FaRegCircle }
 const Services = () => {
     const [services, setServices] = useState([]);
     const [asc, setAsc] = useState(true);
+    const searchRef = useRef(null);
+    const [search, setSearch]= useState('');
     // console.log(services)
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services?sort=${asc ? 'asc':'desc' }`)
+        fetch(`https://car-doctor-server-gamma-eight.vercel.app/services?search=${search}&sort=${asc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [asc])
+    }, [asc, search])
+
+    const handleSearch = () =>{
+        setSearch(searchRef.current.value);
+        console.log(searchRef.current.value);
+    }
+
     return (
         <div>
             <div>
@@ -20,11 +28,20 @@ const Services = () => {
                     <h3 className='text-xl text-red-500'>Services</h3>
                     <h1 className='text-4xl font-extrabold'>Our Service Area</h1>
                     <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+                    <div className="form-control">
+                        <div className="input-group">
+                            <input type="text" ref={searchRef} placeholder="Search…" 
+                            className="input input-bordered" />
+                            <button onClick={handleSearch} className="btn btn-square">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
+                    </div>
                     <button
-                    className='btn-error btn-sm btn-outline border rounded'
-                     onClick={()=>setAsc(!asc)}
-                     >
-                      Price: {asc ? 'High to low' : 'Low to high'}
+                        className='btn-error btn-sm btn-outline border rounded'
+                        onClick={() => setAsc(!asc)}
+                    >
+                        Price: {asc ? 'High to low' : 'Low to high'}
                     </button>
                 </div>
                 <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-6 my-10'>
